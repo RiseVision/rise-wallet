24d69283cc20520ebb9deb281c5784d7171d0082
Andrea Baccega <vekexasia@gmail.com>
2017-08-18 17:30:23 +0200

    removed nodeAddress coercion.

--- js/services/riseService.js
+++ js/services/riseService.js
@@ -1,7 +1,6 @@
 require('angular');
 var riseJs  = require('risejs').rise;
 angular.module('liskApp').service('riseAPI', function () {
-  riseJs.nodeAddress = 'http://localhost:5566';
 
   return riseJs;
 
